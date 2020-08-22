a41ea16e85c41e8b952113b03986c08eb7c90184
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2020-08-22 12:04:21 +0530

    updated form

--- forms/contact.php
+++ forms/contact.php
@@ -23,15 +23,15 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  //  SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'mail.dnyaneshwardake.com',
-    'username' => 'contact@dnyaneshwardake.com',
-    'password' => 'Narmada@3d',
+ <!-- $contact->smtp = array(
+    'host' => '',
+    'username' => '',
+    'password' => '',
     'port' => '587'
   );
-  
+  -->
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
